9ece338faa0a2acef6f827542b869aa17925aa68
Alasdair Smith <alasdairsmith100@gmail.com>
2014-03-20 16:17:59 +0000

    Img segmentation using img subtraction, plus added handling for single img segmentation

--- coursework.m
+++ coursework.m
@@ -7,8 +7,8 @@ im_end = imread('010.jpg');
 color_detect(im_start)
 
 % Segment images using color sample
-im_start_bw = segment_image(im_start);
-im_end_bw = segment_image(im_end);
+im_start_bw = segment_image(im_start, im_end);
+im_end_bw = segment_image(im_end, im_start);
 
 % Test first and last images for speed
 speed_test(im_start_bw, im_end_bw);
--- segment_image.m
+++ segment_image.m
@@ -1,11 +1,26 @@
-function im_output = segment_image(im_input)
+function im_output = segment_image(im_input, im_samp)
 
-% Filter image with average filter
-filter_av = fspecial('average', 20); % Create average filter
-im_gau = imfilter(im_input, filter_av, 'replicate'); % Apply average filter
+% Create average filter
+filter_av = fspecial('average', 10);
 
-% Convert image to black and white
-im_bw_reversed = im2bw(im_gau, 0.22);
+% Test is sample img exists - if not, use LoG filter to segment img. If
+% sample img exists, use img subtraction to filter
+if (strcmp(im_samp, ''))
+    % Filter with average filter
+    im_gau = imfilter(im_input, filter_av, 'replicate');
+    
+    % Convert to black & white
+    im_bw_reversed = im2bw(im_gau, 0.25);
+    im_output = im_bw_reversed < max(im_bw_reversed(:)); % Reverse b/w so foreground becomes background
+else
+    % Subtract sample img from main img
+    im_sub = imsubtract(im_samp, im_input);
 
-% Reverse the b/w img, so foreground becomes background
-im_output = im_bw_reversed < max(im_bw_reversed(:));
+    % Filter with average filter - reduces noise
+    im_gau = imfilter(im_sub, filter_av, 'replicate');
+
+    % Convert to black & white
+    im_output = im2bw(im_gau, 0.3);
+end
+
+% figure, imshow(im_output)
